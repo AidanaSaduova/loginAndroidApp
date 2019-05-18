0cd943825f1e4011ad9df4d01fc50a66df29904b
Aidana <a.saduova@mymovie.guru>
2019-05-18 21:16:12 +0200

    dx: updated login logic using retrofit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,57 +1,59 @@
 package com.example.myapplication;
 
-import android.content.Intent;
-import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeoutException;
-
-import io.swagger.client.ApiException;
-import io.swagger.client.api.DefaultApi;
+import com.example.myapplication.service.UserClient;
 
 import io.swagger.client.model.UserLoginApiForm;
+import io.swagger.client.model.UserViewModel;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    //initialize a variables for a screen form
     private EditText Username;
     private EditText Password;
     private Button Login;
-    private EditText name;
-    private EditText passw;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //declare variables and bind it with xml
+        Username = findViewById(R.id.etUsername);
+        Password = findViewById(R.id.etPassword);
+        Login = findViewById(R.id.btnLogin);
 
-        Username = (EditText)findViewById(R.id.etUsername);
-        Password = (EditText)findViewById(R.id.etPassword);
-        Login = (Button)findViewById(R.id.btnLogin);
+        //initialize Onclick listener for Login button
         Login.setOnClickListener(this);
 
     }
 
 
+    //a method to test inputs and validation on the view
     private void validate(String username, String password){
         if(username.equals("Admin") && password.equals("1234")){
-            //Intent intent = new Intent(MainActivity.this, SecondActivity.class);
             showToast(R.string.loginSuccess);
-            //startActivity(intent);
-
         }else{
             showToast(R.string.loginFailed);
         }
     }
 
 
+    //Show status messages on screen (success or fail)
     public void showToast(final int resId){
         runOnUiThread(new Runnable() {
             @Override
@@ -65,19 +67,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
     }
 
+
+    //Specify a login logic per click
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.btnLogin:
-                validate(Username.getText().toString(),Password.getText().toString());
-                UserLoginApiForm formToSend = wrapToModel(Username.getText().toString(), Password.getText().toString());
-                //validateForm(formToSend);
-
-
+                //call test method
+                //validate(Username.getText().toString(),Password.getText().toString());
+                final UserLoginApiForm form = wrapToModel(Username.getText().toString(), Password.getText().toString());
+                sendNetworkRequest(form);
         }
-
     }
 
+
+    private void sendNetworkRequest(UserLoginApiForm form) {
+        //create retrofit instance
+        Retrofit.Builder builder = new Retrofit.Builder()
+                .baseUrl("http://10.0.2.2:9000/api/")
+                .addConverterFactory(GsonConverterFactory.create());
+
+        Retrofit retrofit = builder.build();
+
+        //get client and call an object for the request
+        UserClient client = retrofit.create(UserClient.class);
+        Call<UserViewModel> call = client.login(form);
+
+        call.enqueue(new Callback<UserViewModel>() {
+            @Override
+            public void onResponse(Call<UserViewModel> call, Response<UserViewModel> response) {
+                Toast.makeText(MainActivity.this,"Success, User-token:"+response.body().getToken(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<UserViewModel> call, Throwable t) {
+                Toast.makeText(MainActivity.this,"something went wrong :(", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    //Helper method, to wrap typed Username and password to the swagger Model
     private UserLoginApiForm wrapToModel(String username, String password){
         UserLoginApiForm form = new UserLoginApiForm();
         form.setName(username);
@@ -86,40 +116,68 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
-    private void validateForm(final UserLoginApiForm form){
-        DefaultApi api = new DefaultApi();
-        try {
+    // 1st try to use Default api from swagger, added it in the Thread as recommended in the docs
+    //result: failed to handle threads -> The application may be doing too much work on its main thread.
 
-            if(!api.login(form).getToken().isEmpty()){
-                showToast(R.string.loginSuccess);
-            }else {
-                showToast(R.string.loginFailed);
+   /* private void validateForm(final UserLoginApiForm form){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                DefaultApi apiInstance = new DefaultApi();
+                try {
+                    UserViewModel result = apiInstance.login(form);
+                    System.out.println(result);
+                } catch (ApiException e) {
+                    System.err.println("Exception when calling DefaultApi#login");
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                } catch (TimeoutException e) {
+                    e.printStackTrace();
+                }
             }
-        } catch (TimeoutException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ApiException e) {
-            e.printStackTrace();
-        }
 
-    }
+        });
 
+    }*/
 
-    private class DownloadFilesTask extends AsyncTask<Void, Void, Void> {
-        protected Void doInBackground(Void... urls) {
+    // 2nd try is to handle the Default api in a Async task
+    //result: also fail, put the timer to wait execution, but doesn't react
+    /*private class checkLogin extends AsyncTask<String, Void, UserViewModel> {
+        protected UserViewModel doInBackground(String... urls) {
            //TODO: implement Api call
-            return null;
+            UserLoginApiForm user = wrapToModel(Username.getText().toString(), Password.getText().toString());
+            DefaultApi apiInstance = new DefaultApi();
+            UserViewModel result = new UserViewModel();
+            try {
+                result = apiInstance.login(user);
+                Log.d("Result",result.getToken());
+
+            } catch (TimeoutException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ApiException e) {
+                e.printStackTrace();
+            }
+            Log.d("Result", result.getToken());
+            return result;
+
         }
 
-
-
-        protected void onPostExecute(Void result) {
+        protected void onPostExecute(UserViewModel result) {
             //TODO update GUI result with the result of do in background
+            if(result.getToken().isEmpty()){
+                showToast(R.string.loginFailed);
+            } else{
+                showToast(R.string.loginSuccess);
+            }
         }
 
-    }
+    }*/
 
 }
